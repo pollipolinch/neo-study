4c3c4ebe7e12b6638ebbee6507fd025bbed3e769
pollipolinch <pollifriend@mail.ru>
2023-05-11 01:32:09 +0500

    feat: add img

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -89,7 +89,7 @@
                 <img
                   v-if="product.activeBuy"
                   class="price-container-simbol"
-                  src="https://profsvarka.msk.ru/wp-content/uploads/2022/04/%D0%BE%D1%80%D0%B0%D0%BD%D0%B6%D0%B5%D0%B2%D0%B0%D1%8F-%D0%B3%D0%B0%D0%BB%D0%BE%D1%87%D0%BA%D0%B0.png"
+                  src="../assets/check-mark.png"
                   alt=""
                 />
                 <h4
